d2efb3bd847c2f1600690d767dd550ec2144d9ed
LouisWood <46609485+LouisWood@users.noreply.github.com>
2022-04-11 23:58:31 +0200

    Ajout du css

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,20 +1,16 @@
-import { Tab, Row, Col, Nav } from 'react-bootstrap'
+import { Tab, Nav } from 'react-bootstrap'
 
 const Dashboard = () => {
   return (
-    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-      <Row>
-        <Col sm={2}>
-          <Nav variant="pills" className="flex-column">
-            <Nav.Item>
-              <Nav.Link eventKey="first">Tab 1</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="second">Tab 2</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Col>
-      </Row>
+    <Tab.Container defaultActiveKey="first">
+      <Nav variant="pills" className="flex-column">
+        <Nav.Item>
+          <Nav.Link eventKey="first">Tab 1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="second">Tab 2</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </Tab.Container>
   )
 }
