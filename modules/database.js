67ea1381889fd0f034a17c3f386fe759a2bfd5a4
LouisWood <46609485+LouisWood@users.noreply.github.com>
2022-05-23 10:36:56 +0200

    Back

--- modules/database.js
+++ modules/database.js
@@ -11,7 +11,10 @@ const knex = require('knex')({
       user : process.env.DB_USER,
       password : process.env.DB_PASSWORD,
       database : process.env.DB_DATABASE,
-      ssl: false
+      ssl: {
+        rejectUnauthorized: false,
+        requestCert: true
+      }
     }
 })
 
